0b8f038f9e34c8c65e2a81c1b9a212cb6ae46099
Laura de Oliveira <l4ur4.88@gmail.com>
2026-07-28 10:56:57 -0300

    fix: handle `client` as `undefined` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -9,11 +9,12 @@ async function query(queryObject) {
 
     return result;
   } catch (error) {
+    console.log("\nErro dentro do catch do database.js...");
     console.error(error);
 
     throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
